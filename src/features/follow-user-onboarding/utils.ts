820e4588cd927f2fab6368c3ac22c613ea4a23a0
Roman Klimov <raklimov@sberbank.ru>
2023-05-28 23:14:11 +0300

    reports list unauth display fix

--- src/features/follow-user-onboarding/utils.ts
+++ src/features/follow-user-onboarding/utils.ts
@@ -1,5 +1,6 @@
 import { calculation_state } from "@states/calculation";
 import { Calculation } from "@tyles/calculation";
+import { useEffect, useState } from "react";
 import { useRecoilValue } from "recoil";
 
 export const useStoragedCalc = ():
@@ -8,15 +9,22 @@ export const useStoragedCalc = ():
   const calc = localStorage.getItem("report");
   const data = localStorage.getItem("snapshot");
   const calculation = useRecoilValue(calculation_state);
-  if (calc && data) {
-    const ser_calc: { from: string; to: string } = JSON.parse(calc);
-    const ser_data: Calculation = JSON.parse(data);
-    return {
-      from: ser_calc.from,
-      to: ser_calc.to,
-      ...ser_data,
-      ...calculation.snapshot,
-    };
-  }
-  return null;
+  const [storaged, setStoraged] = useState<
+    null | ({ from: string; to: string } & Calculation)
+  >(null);
+
+  useEffect(() => {
+    if (calc && calculation.snapshot) {
+      const ser_calc: { from: string; to: string } = JSON.parse(calc);
+      //   const ser_data: Calculation = JSON.parse(data);
+      //   console.log(ser_calc, ser_data);
+      setStoraged({
+        from: ser_calc.from,
+        to: ser_calc.to,
+        // ...ser_data,
+        ...calculation.snapshot,
+      });
+    }
+  }, [calc, calculation.snapshot]);
+  return storaged;
 };
